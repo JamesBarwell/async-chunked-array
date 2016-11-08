89297769416fead776cdd042dbc4764c809dd781
James Barwell <jb@jamesbarwell.co.uk>
2016-11-08 23:04:32 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,47 @@
+'use strict'
+
+module.exports = {
+    each: each,
+    sort: sort
+}
+
+function each(collection, chunkLength, operation, done) {
+    function actOnRange(startIndex, doneChunk) {
+        let limit = startIndex + chunkLength
+
+        for (let i = startIndex; i < limit && i < collection.length; i++) {
+            operation(collection[i])
+        }
+
+        if (limit > collection.length) {
+            return doneChunk(null, collection)
+        } else {
+            setImmediate(() => {
+                actOnRange(limit, done)
+            })
+        }
+    }
+
+    actOnRange(0, done)
+}
+
+function sort(collection, chunkLength, sortFunc, done) {
+    const iterationMax = Math.ceil(collection.length / chunkLength)
+
+    function actOnRange(iteration, doneChunk) {
+        if (iteration > iterationMax) {
+            let finalSort = collection.sort(sortFunc)
+            return doneChunk(null, finalSort)
+        }
+
+        let chunk = collection.splice(0, chunkLength)
+        chunk.sort(sortFunc)
+        collection = collection.concat(chunk)
+
+        setImmediate(() => {
+            actOnRange(iteration + 1, doneChunk)
+        })
+    }
+
+    actOnRange(0, done)
+}
---
+++ test/main.js
@@ -0,0 +1,76 @@
+'use strict'
+
+const assert = require('assert')
+const arrayHelper = require('../index')
+
+describe('module', () => {
+
+    describe('each', () => {
+        let actual = []
+        let expected = []
+
+        beforeEach((done) => {
+            const longArray = range(0, 100000)
+            const transformExpected = function(item) {
+                if (item % 1000 === 0) {
+                    expected.push(item)
+                }
+            }
+            const transformActual = function(item) {
+                if (item % 1000 === 0) {
+                    actual.push(item)
+                }
+            }
+
+            cloneArray(longArray).forEach(transformExpected)
+
+            arrayHelper.each(longArray, 1000, transformActual, done)
+        })
+
+        it('runs a function on each array item', () => {
+            assert.deepEqual(actual, expected)
+        })
+    })
+
+    describe('sort', () => {
+        let actual
+        let expected
+
+        beforeEach((done) => {
+            const longArray = range(0, 100000)
+            const sortFunc = function(a, b) {
+                if (a > b) {
+                    return 1
+                }
+                if (a < b) {
+                    return -1
+                }
+                return 0
+            }
+
+            expected = cloneArray(longArray).sort(sortFunc)
+
+            arrayHelper.sort(longArray, 1000, sortFunc, (err, sorted) => {
+                actual = sorted
+                done()
+            })
+        })
+
+        it('can sort an array', () => {
+            assert.deepEqual(actual, expected)
+        })
+    })
+
+})
+
+function range(start, count) {
+    let rtn = []
+    for (let i = 0; i < count; i++) {
+        rtn.push(i)
+    }
+    return rtn
+}
+
+function cloneArray(a) {
+    return a.slice()
+}
